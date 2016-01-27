097d325ecbf4be6630147315e190d9828499313c
Rafael Mitre <rafael.mitre@gmail.com>
2016-01-27 01:26:07 -0200

    atualização 27-01 >> concluido

--- trabalho2.pl
+++ trabalho2.pl
@@ -1,7 +1,9 @@
 use warnings;
 use strict; #implementar módulos próprios depois caso dê tempo, trabalho ficar mais bonito!! =)
+use File::Copy qw(move);
 
 #SUBROTINAS
+#Contar Caracteres-----------------------------------------------------------------
 sub contarCaracteres {
    my ($texto, $limite) = @_;
    my $saida = 0; # saida recebe 0 se estiver ok, 1 se for muito grande
@@ -20,41 +22,50 @@ sub contarCaracteres {
    return ($saida);
 }
 
+#Filtro parametros: arquivo, filtro, nome do arquivo ----------------------------------------
 sub filtrarTexto {
-   my ($texto, $filtro) = @_;
-   open (my $arqMod, '>', 'arquivoModificado.txt');
+   my ($texto, $filtro, $nomeArq) = @_;
    my @palavroes = <$filtro>;
-   while (<$texto>){
-   my $linha = $_;
-   	foreach my $i (0..($#palavroes-1)){
+   open (my $escrita, '>>', 'arquivo2.txt');
+   
+     while (<$texto>)
+     {
+     my $linha = $_;
+        foreach my $i (0..($#palavroes-1))
+	{
   	chomp ($palavroes[$i]); #tirar o \n do final >> deve ser padrão no arquivo filtro.txt 
-	$linha =~ s/($palavroes[$i])/\*\*\*\*/gi;
+ 	$linha =~ s/($palavroes[$i])/\*\*\*\*/gi;
 	}
-   print $arqMod $linha;
-   }
-   
-   
-  #ler o arquivo texto e passar o filtro
+     print $escrita $linha;
+     print $linha;
+     }
+ 
+  move 'arquivo2.txt', $nomeArq; 
 }
-#PROGRAMA PRINCIPAL----------------------------------------------------
 
-open (my $arquivo, "<", "arquivo.txt") or die "Impossível abrir arquivo.txt"; 
+#PROGRAMA PRINCIPAL----------------------------------------------------
+print "Digite nome do arquivo: ";
+my $nomeArq = <STDIN>;
+chomp $nomeArq; #depois só passar o nome que foi pego com c++ para essa variavel, nao precisa mudar nada no resto do codigo!
+
+open (my $arquivo, "<", $nomeArq) or die "Impossível abrir arquivo.txt"; 
 open (my $filtro, "<", "filtro.txt") or die "impossivel abrir o filtro";
-filtrarTexto ($arquivo, $filtro);
+
+filtrarTexto ($arquivo, $filtro, $nomeArq);
 
 print "Digite o tamanho do arquivo >> ";
 my $lim = <STDIN>;
 seek ($arquivo, 0, 0);
-if (contarCaracteres($arquivo, $lim) == 0)
+     if (contarCaracteres($arquivo, $lim) == 0)
      {
      print "tamanho adequado!! \n";
      }
 
-else  #nesse caso, o arquivo será vetado ou a gente corta o que for muito grande?? 
+     else  #nesse caso, o arquivo será vetado ou a gente corta o que for muito grande?? 
      {
      print "arquivo muito grande!\n"
      }
 
-close $arquivo or die "Impossível fechar arquivo.txt"
-
+close $arquivo or die "Impossível fechar arquivo.txt";
+close $filtro or die "impossível fechar filtro.txt";
 		
